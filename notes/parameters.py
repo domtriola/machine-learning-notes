f9b142ba2ea0886acdb1f5a1449a8483e76990fc
Dominick Triola <dominick@ignovadesign.com>
2016-10-01 17:54:04 -0700

    add parameters note

---
+++ notes/parameters.py
@@ -0,0 +1,24 @@
+########## Parameters ##########
+################################
+
+## kernel
+# The "kernel trick" takes a low dimensional input space and maps it to a high
+# dimensional input space so that previously non-linearly seperable problems
+# become linearly seperable.
+
+# The rbf kernel is usually the default for svm.
+
+
+## C
+# The C parameter controls the tradeoff between making a smooth decision boundary
+# and classifying more training points correctly.
+
+# A larger C value will classify more training points correctly, but it might
+# not create a smooth decision boundary.
+
+
+## gamma
+# The gamma parameter controls how far the influence of a single training point
+# reaches.
+
+# A higher gamma value results in a closer range of influence.
